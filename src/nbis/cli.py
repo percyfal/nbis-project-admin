4b28baade5e7a2c91bdf36182b097eebf961105d
Per Unneberg <per.unneberg@scilifelab.se>
2022-06-11 16:32:43 +0200

    Restructure cli module

--- src/nbis/cli.py
+++ src/nbis/cli.py
@@ -41,37 +41,34 @@ class DescriptionArgumentParser(ArgumentParser):
 
 
 
-def get_nbis_parser(arg_list=None):
-    if arg_list is None:
-        arg_list = sys.argv[1:]
-
+def get_nbis_parser():
     top_parser = DescriptionArgumentParser(
         description=__doc__,
         prog='nbis'
     )
-
-    top_parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
-    top_parser.add_argument('--debug', action='store_true', default=False, help='Print debug messages')
-
-    return top_parser, arg_list
-
-
-def add_subcommand_parser(parser, arg_list=None):
-    subcommand_name = get_subcommand_name(arg_list)
-    module = importlib.import_module("." + subcommand_name, subcommands.__name__)
-    subparsers = parser.add_subparsers()
-    subparser = subparsers.add_parser(
-        subcommand_name, help=module.__doc__.split("\n", maxsplit=1)[1], description=module.__doc__
+    top_parser.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s ' + __version__
+    )
+    top_parser.add_argument(
+        '--debug',
+        action='store_true',
+        default=False,
+        help='Print debug messages'
     )
 
-    module.add_arguments(subparser)
-    return parser
+    return top_parser
 
 
 def main(arg_list=None):
+    if arg_list is None:
+        arg_list = sys.argv[1:]
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    parser, arg_list = get_nbis_parser(arg_list)
-    parser = add_subcommand_parser(parser, arg_list)
+    minimal_parser = make_minimal_parser(subcommands)
+    subcommand_name = get_subcommand_name(minimal_parser, arg_list)
+    parser = make_subcommand_parser(subcommand_name)
+
     args = parser.parse_args(arg_list)
 
     if args.debug:
@@ -83,35 +80,48 @@ def main(arg_list=None):
     return 0
 
 
-def get_subcommand_name(arguments) -> str:
-    """
-    Parse arguments to find out which subcommand was requested.
-
-    This sets up a minimal ArgumentParser with the correct help strings.
-
-    Because help is obtained from a module’s docstring, but importing each module
-    makes startup slow, the modules are only parsed with the ast module and
-    not fully imported at this stage.
-
-    Return:
-        subcommand name
-    """
-    parser = DescriptionArgumentParser(description=__doc__, prog="nbis")
-    parser.add_argument("--version", action="version", version=__version__)
-    subparsers = parser.add_subparsers()
-
-    # Only looks at subcommands directory!
-    for module_name, docstring in subcommands_modules(subcommands):
-        help = docstring.split("\n", maxsplit=1)[1].replace("%", "%%")
-        subparser = subparsers.add_parser(
-            module_name, help=help, description=docstring, add_help=False
-        )
-        subparser.set_defaults(module_name=module_name)
-    args, _ = parser.parse_known_args(arguments)
+def get_subcommand_name(parser, arg_list) -> str:
+    """Parse arg_list to get name of subcommand"""
+    args, _ = parser.parse_known_args(arg_list)
     module_name = getattr(args, "module_name", None)
     if module_name is None:
         parser.error("Please provide the name of a subcommand to run")
     return module_name
+
+
+def make_subcommand_parser(subcommand_name):
+    """
+    Make parser and load module whilst adding subparser documentation.
+    """
+    parser = get_nbis_parser()
+    subparsers = parser.add_subparsers()
+    module = importlib.import_module("." + subcommand_name, subcommands.__name__)
+    subparser = subparsers.add_parser(
+        subcommand_name, help=module.__doc__.split("\n", maxsplit=1)[1], description=module.__doc__
+    )
+    subparser.set_defaults(module_name=subcommand_name, runner=module.main)
+    module.add_arguments(subparser)
+    return parser
+
+
+# We need the minimal parser just to get the actual subcommand name
+def make_minimal_parser(package_list):
+    """
+    Make minimal parser including subcommands from package_list
+    """
+    parser = get_nbis_parser()
+    subparsers = parser.add_subparsers()
+
+    if not isinstance(package_list, list):
+        package_list = [package_list]
+    for pkg in package_list:
+        for module_name, docstring in subcommands_modules(pkg):
+            help = docstring.split("\n", maxsplit=1)[1].replace("%", "%%")
+            subparser = subparsers.add_parser(
+                module_name, help=help, description=docstring, add_help=False
+            )
+        subparser.set_defaults(module_name=module_name)
+    return parser
 
 
 def subcommands_modules(package):
---
+++ tests/test_cli.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import pytest
+import importlib
+import argparse
+from importlib.machinery import SourceFileLoader
+import subprocess
+from nbis import subcommands
+from nbis.cli import subcommands_modules, make_minimal_parser, make_subcommand_parser
+
+
+def add_module(p, pkg, modules):
+    pout = p / pkg
+    pout.mkdir()
+    for mod in modules:
+        fn = f"__{mod}__.py" if mod == "init" else f"{mod}.py"
+        out = pout / fn
+        out.write_text(f'"""\n{mod}\n"""')
+    return pout
+
+
+@pytest.fixture
+def package(tmp_path):
+    pout = add_module(tmp_path, "package", ["init"])
+    add_module(pout, "commands", ["init", "foo", "bar"])
+    add_module(pout, "utils", ["init", "foobar", "barfoo"])
+    package = SourceFileLoader(fullname="package", path=str(pout / "__init__.py")).load_module()
+    return package
+
+
+@pytest.fixture
+def commands(package):
+    from package import commands
+    return commands
+
+
+@pytest.fixture
+def utils(package):
+    from package import utils
+    return utils
+
+@pytest.fixture
+def webexport(monkeypatch):
+    from nbis.subcommands import webexport
+    def mockreturn(cls):
+        return "Mock webexport runner"
+    monkeypatch.setattr(webexport, 'main', mockreturn)
+
+
+
+def test_subcommands_modules(commands):
+    modules = dict((mod, docstring.strip()) for mod, docstring in
+                   subcommands_modules(commands))
+    assert sorted(modules.keys()) == ["bar", "foo"]
+    assert sorted(modules.values()) == ["bar", "foo"]
+
+
+def test_make_minimal_parser(commands, utils):
+    parser = make_minimal_parser([commands, utils])
+    subcommands = []
+    for action in parser._action_groups:
+        for act in action._actions:
+            if isinstance(act, argparse._SubParsersAction):
+                subcommands.extend(act.choices.keys())
+    assert set(subcommands) == {'bar', 'foo', 'barfoo', 'foobar'}
+
+
+def test_webexport_subcommand(webexport):
+    parser = make_subcommand_parser("webexport")
+    args = parser.parse_args(["webexport"])
+    results = args.runner(args)
+    assert results == "Mock webexport runner"
