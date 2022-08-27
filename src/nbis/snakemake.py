9e61e9afb181e7443a58fcc4ca398b82ca2369a7
Per Unneberg <per.unneberg@scilifelab.se>
2022-08-27 22:08:45 +0200

    Add utility function to add snakemake arguments

--- src/nbis/snakemake.py
+++ src/nbis/snakemake.py
@@ -15,3 +15,39 @@ def profile(uri, config):
     finally:
         logger.debug(f"trying snakemake profile at uri '{uri}'")
     return uri
+
+def add_arguments(parser, profile=True, no_profile=True, jobs=True, test=True):
+    """Utility function to add some standard arguments to snakemake parser"""
+    if profile:
+        parser.add_argument(
+            "--profile",
+            action="store",
+            default="rackham",
+            help=(
+                "snakemake profile, either defined as key:value pair in config"
+                " or a URI pointing to profile directory"
+            ),
+        )
+    if no_profile:
+        parser.add_argument(
+            "--no-profile",
+            action="store_true",
+            default=False,
+            help="disable snakemake profile",
+        )
+    if jobs:
+        parser.add_argument(
+            "-j", "--jobs",
+            action="store",
+            default=1,
+            help="snakemake jobs"
+        )
+    if test:
+        parser.add_argument(
+            "--test",
+            action="store_true",
+            default=False,
+            help="run workflow on small test data set",
+        )
+
+
