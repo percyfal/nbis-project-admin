88762b9e6fb061b08e66cf248ba251f19083415e
Per Unneberg <per.unneberg@scilifelab.se>
2023-01-27 11:55:27 +0100

    Reformat files

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,7 +5,7 @@ expected = [
     "project_foo/.editorconfig",
     "project_foo/.pre-commit-config.yaml",
     "project_foo/.prettierignore",
-    "project_foo/.prettierrc.yaml",
+    "project_foo/.prettierrc.yml",
     "project_foo/README.md",
     "project_foo/setup.cfg",
     "project_foo/pyproject.toml",
